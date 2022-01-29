5a8881903146b8219bbaeb2cd1322272d0ec3290
Christian Dimitri <chriistian.j.dimitrii@gmail.com>
2022-01-29 20:22:10 +0100

    material assets updated and Movement class binding inputs working

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -7,12 +7,33 @@ public class Movement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    private void ProcessRotation()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("Pressed A - Rotating Left");
+        }
+
+        else if (Input.GetKey(KeyCode.D))
+        {
+            Debug.Log("Pressed D - Rotating Right");
+        }
+    }
+
+    void ProcessThrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log("Pressed SPACE - Thrusting");
+        }
     }
 }
