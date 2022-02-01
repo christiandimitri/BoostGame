3577b7d57ed1d4d7500332654363da34bfc3e792
Christian Dimitri <chriistian.j.dimitrii@gmail.com>
2022-02-01 20:51:42 +0100

    Oscillator class added

---
+++ Assets/Scripts/Oscillator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class Oscillator : MonoBehaviour
+    {
+        Vector3 startingPosition;
+        [SerializeField] Vector3 movementVector;
+
+        [SerializeField] [Range(0, 1)] float movementFactor;
+        [SerializeField] float period = 2f;
+
+        private void Start()
+        {
+            startingPosition = transform.position;
+        }
+
+        private void Update()
+        {
+            float cycles = Time.time / period;
+
+            const float tau = Mathf.PI * 2;
+            float rawSinWave = Mathf.Sin(tau * cycles);
+            movementFactor = (rawSinWave + 1f) / 2f;
+            Vector3 offset = movementVector * movementFactor;
+            transform.position = startingPosition + offset;
+        }
+    }
+}
