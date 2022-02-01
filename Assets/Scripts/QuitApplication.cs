33326a227efcdae1c01fcfb613b756ab447ea86a
Christian Dimitri <chriistian.j.dimitrii@gmail.com>
2022-02-01 21:03:40 +0100

    Application quit method trigger on Escape key down

---
+++ Assets/Scripts/QuitApplication.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class QuitApplication : MonoBehaviour
+    {
+        private void Update()
+        {
+            ApplyQuitApplication();
+        }
+
+        private void ApplyQuitApplication()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Debug.Log("We pushed escape");
+                Application.Quit();
+            }
+        }
+    }
+}
